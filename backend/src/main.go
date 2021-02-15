ef95e5653ae72ba7b0e8d5113b4eef4eb81b14e3
Baptiste Andre <bandre@profield.io>
2021-02-15 18:59:15 +0100

    deployments

--- backend/src/main.go
+++ backend/src/main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"log"
 	"net/http"
+	"runtime"
 
 	"./database"
 	"./user"
@@ -12,12 +15,39 @@ import (
 func getArticle(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	key := vars["id"]
-	fmt.Fprintf(w, "key: "+key)
+	fmt.Printf(key, "test")
+}
+
+func RecoverWrap(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		var err error
+
+		defer func() {
+			r := recover()
+			if r != nil {
+
+				switch t := r.(type) {
+				case string:
+					err = errors.New(t)
+				case error:
+					err = t
+				default:
+					err = errors.New("Unknown error")
+				}
+
+				_, fn, line, _ := runtime.Caller(2)
+				log.Printf("[error] %s:%d %v", fn, line, err)
+
+				http.Error(w, "Internal serveur error", http.StatusInternalServerError)
+			}
+
+		}()
+		next.ServeHTTP(w, r)
+	})
 }
 
 func LogMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Prints logs")
 		fmt.Println(r.Method, r.URL)
 		next.ServeHTTP(w, r)
 	})
@@ -25,15 +55,17 @@ func LogMiddleware(next http.Handler) http.Handler {
 
 func handleRequest() {
 	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.Use(RecoverWrap)
 	user.GetUserRouter(myRouter)
-	// s := myRouter.PathPrefix("/article").Subrouter()
-	// s.Use(user.AuthMiddleware)
-	// s.HandleFunc("/{id}", getArticle)
+
+	myRouter.Use(user.AuthMiddleware)
+	myRouter.HandleFunc("/articles", getArticle)
 	mw := LogMiddleware(myRouter)
 	http.ListenAndServe(":3000", mw)
 }
 
 func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
 	client := database.OpenMongoClient()
 	defer client.Disconnect(database.DatabaseCtx)
 	handleRequest()
--- backend/src/user/user_test.go
+++ backend/src/user/user_test.go
@@ -3,8 +3,11 @@ package user
 import (
 	"bytes"
 	"encoding/json"
+	"fmt"
 	"io/ioutil"
 	"net/http"
+	"net/http/cookiejar"
+	"net/url"
 	"testing"
 
 	"../database"
@@ -36,17 +39,45 @@ import (
 // }
 
 func TestLogin(t *testing.T) {
+
 	user := database.User{Email: "baptiste", Password: "test"}
 	parameters, _ := json.Marshal(user)
-	http.Post("http://localhost:3000/signup", "application/json", bytes.NewBuffer(parameters))
-	resp, err := http.Post("http://localhost:3000/login", "application/json", bytes.NewBuffer(parameters))
+	resp, err := http.Post("http://localhost:3000/signup", "application/json", bytes.NewBuffer(parameters))
 	if err != nil {
 		t.Fatal(err)
 	}
-	ioutil.ReadAll(resp.Body)
-
 	if resp.StatusCode != http.StatusOK {
-		t.Fatal("statuscode")
+		t.Fatal("Wrong status code")
+	}
+	resp, err = http.Post("http://localhost:3000/login", "application/json", bytes.NewBuffer(parameters))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.StatusCode != http.StatusOK {
+		t.Fatal("Wrong status code")
+	}
+	var jwtToken string
+	for _, cookie := range resp.Cookies() {
+		fmt.Println("Found a cookie named:", cookie.Name, cookie.Value)
+		jwtToken = cookie.Value
+	}
+	jar, _ := cookiejar.New(nil)
+	var cookies []*http.Cookie
+
+	cookie := &http.Cookie{
+		Name:  "token",
+		Value: jwtToken,
+	}
+	cookies = append(cookies, cookie)
+	u, _ := url.Parse("http://localhost:300")
+	jar.SetCookies(u, cookies)
+	client := &http.Client{
+		Jar: jar,
+	}
+	client.Get("http://localhost:3000/articles")
+	ioutil.ReadAll(resp.Body)
+	if resp.StatusCode != http.StatusOK {
+		t.Fatal("Wrong status code")
 	}
 
 }
